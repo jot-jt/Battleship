e0820e4597d742531442dda5359b8437c19feef7
jot-jt <91110564+jot-jt@users.noreply.github.com>
2023-01-14 15:42:26 -0500

    Rename bugfix

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,8 +14,8 @@ public class Main {
         Board gameboardOne= new Board(BOARD_WIDTH, BOARD_HEIGHT);
         Board gameboardTwo= new Board(BOARD_WIDTH, BOARD_HEIGHT);
 
-        Player playerOne= new Player("Player 1", gameboard1, gameboard2);
-        Player playerTwo= new Player("Player 2", gameboard2, gameboard1);
+        Player playerOne= new Player("Player 1", gameboardOne, gameboardTwo);
+        Player playerTwo= new Player("Player 2", gameboardTwo, gameboardOne);
 
         ShipPlacer.placeShipsOnBoard(playerOne);
         ShipPlacer.placeShipsOnBoard(playerTwo);
